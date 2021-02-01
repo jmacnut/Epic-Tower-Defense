c3c31d1c8e95c9fc09c7a0473060692bbc7507ed
jmacnut <jmacnut@gmail.com>
2021-02-01 05:18:57 -1000

    ref a little ai scene

---
+++ Assets/Ready_For_A_Little_AI/LittleAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LittleAI : MonoBehaviour
+{
+    // define target
+    [SerializeField] private GameObject _target;
+
+    // NavMeshAgent handle
+    [SerializeField] private NavMeshAgent _agent;
+
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.SetDestination(_target.transform.position);
+    }
+
+    void Update()
+    {
+        // use NavMesh Agent to move toward the target
+        // Docs: NavMeshAgent.SetDestination
+
+    }
+}
