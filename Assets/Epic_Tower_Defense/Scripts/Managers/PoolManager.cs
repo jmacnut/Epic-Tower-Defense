8d2665a8d4cab0e9b4e93b46e5e22bd02f90a3b7
jmacnut <jmacnut@gmail.com>
2021-02-04 05:21:25 -1000

    begins pool manager

--- Assets/Epic_Tower_Defense/Scripts/Managers/PoolManager.cs
+++ Assets/Epic_Tower_Defense/Scripts/Managers/PoolManager.cs
@@ -1,21 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace ETD.Scripts.Managers
 {
     public class PoolManager : MonoBehaviour
     {
-        // Start is called before the first frame update
+        // data structures - Doc: Unity Collections and Data Structures
+        //private List<GameObject> _enemyPoolList;    // Lists: can pull from middle
+
+        // recommended
+        private Queue<GameObject> _enemyPoolQueue;  // Queues: FIFO
+        //private Stack<GameObject> _enemyPoolStack;  // Stacks: LIFO
+
         void Start()
+        {
+            //_enemyPoolList[0];           // first element in list
+            //_enemyPoolList.Remove();     // remove item from list
+
+            // generate list of enemies and populate the pool
+
+            _enemyPoolQueue.Peek();      // view first element i queue
+            _enemyPoolQueue.Enqueue();   // append to tail
+            _enemyPoolQueue.Dequeue();   // remove from head
+        
+        }
+
+        void Update()
         {
 
         }
 
-        // Update is called once per frame
-        void Update()
+        public GameObject RequestEnemy()
         {
+            // find first enemy within the enemy pool
+            // that is turned off and return it to the spawn manager
+            // Note: for() loops are more optimal than foreach() loops
+            //foreach (var enemy in _enemyPoolQueue)
+            //{
+            //    if (enemy.activeInHierarchy == false)
+            //    {
+            //        return enemy;
+            //    }
+            //}
 
+            // more efficient to use LINQ query instead of foreach
+            // returns bool, requires a predicate (delegate function)
+            var enemy = _enemyPoolQueue.First(enemy => enemy.activeInHierarchy == false);
+
+            // failsafe - create a new enemy
+            var newEnemy = new GameObject("Enemy");
+            _enemyPoolQueue.Enqueue(newEnemy);
+            return newEnemy;
         }
     } 
 }
